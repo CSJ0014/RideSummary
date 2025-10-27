9a5a6858b5c2f0f63ae096c48e1dc2afb9649a12
Caleb <CSJ0014@gmail.com>
2025-10-27 10:19:31 -0400

    Delete pages directory

--- pages/api/auth.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const clientId = process.env.STRAVA_CLIENT_ID;
-  const redirect = process.env.REDIRECT_URI;
-  const scope = "activity:read_all";
-  const url = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirect}&response_type=code&scope=${scope}`;
-  res.redirect(url);
-}
--- pages/api/rides.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const { token } = req.query;
-  const actResp = await fetch("https://www.strava.com/api/v3/athlete/activities?per_page=20", {
-    headers: { Authorization: `Bearer ${token}` }
-  });
-  const activities = await actResp.json();
-  res.status(200).json(activities);
-}
